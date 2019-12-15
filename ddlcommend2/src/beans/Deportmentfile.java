e0eee897769d0513fcb942acf123e9b8608276e3
chandrasekharD3 <chandra.pandari12@gmail.com>
2019-12-15 10:00:41 +0530

    second ddl presist and create

---
+++ ddlcommend2/src/beans/Deportmentfile.java
@@ -0,0 +1,33 @@
+package beans;
+
+public class Deportmentfile {
+private int did;
+private String dname;
+private String ddes;
+private int dsal;
+public int getDid() {
+	return did;
+}
+public void setDid(int did) {
+	this.did = did;
+}
+public String getDname() {
+	return dname;
+}
+public void setDname(String dname) {
+	this.dname = dname;
+}
+public String getDdes() {
+	return ddes;
+}
+public void setDdes(String ddes) {
+	this.ddes = ddes;
+}
+public int getDsal() {
+	return dsal;
+}
+public void setDsal(int dsal) {
+	this.dsal = dsal;
+}
+
+}
